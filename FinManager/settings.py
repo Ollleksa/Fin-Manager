9e11162ffa127eeddd1c706fe61bb92bea047fa0
Oleksii Bunyk <oleksabunyk@gmail.com>
2019-01-16 16:08:53 +0200

    Login page by wsvincent.com

--- FinManager/settings.py
+++ FinManager/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'FinManager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -112,6 +112,8 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+LOGIN_REDIRECT_URL = '/'
 
 
 # Static files (CSS, JavaScript, Images)
