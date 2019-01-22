cea2df5c9c5e2d2a5627c2b28b80a40c4a616e8a
Oleksii Bunyk <oleksabunyk@gmail.com>
2019-01-22 14:39:05 +0200

    It actually take info from database. Yes!

--- FinManager/settings.py
+++ FinManager/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
-    'balance',
+    'balance.apps.BalanceConfig',
 ]
 
 MIDDLEWARE = [
--- balance/models.py
+++ balance/models.py
@@ -4,8 +4,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Balance(models.Model):
-    balance_count = models.DecimalField(max_digits=20, decimal_places=2, default=0.0)
-    name = models.CharField(max_length=80, default=User.username)
+    balance_count = models.DecimalField(max_digits=20, decimal_places=2)
+    name = models.CharField(max_length=80)
 
     def __str__(self):
-        return self.user_name + ':   ' + str(self.balance_count)
+        description = str(self.name) + '   ' + str(self.balance_count)
+        return description
