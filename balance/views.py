c5590465f452b20748069317f9d631aaa20858b8
Oleksii Bunyk <oleksabunyk@gmail.com>
2019-01-24 22:15:51 +0200

    It update value form home site

--- balance/views.py
+++ balance/views.py
@@ -13,3 +13,24 @@ def index(request):
     #output = 'AAAAAAAAAAAA:   ' + str(money_list[0])
     #return HttpResponse(TemplateView.as_view(template_name='home.html'))
     return HttpResponse(template.render(context,request))
+
+from .forms import Update_Balance
+
+def update_bal(request):
+    if request.method == 'POST':
+        form = Update_Balance(request.POST)
+        if form.is_valid():
+            b = Balance.objects.get(id=1)
+            b.balance_count = form.cleaned_data['new_balance']
+            b.save()
+
+    else:
+        form = Update_Balance()
+
+    money_list = Balance.objects.all()
+    template = loader.get_template('current_balance.html')
+    context = {
+        'money_list': money_list,
+        'form': form
+    }
+    return HttpResponse(template.render(context, request))
